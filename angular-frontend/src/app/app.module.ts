758cda557d2f1afdf0f7f5d9467b9ae6e3b1cae3
THI TRIEU XUYEN NGUYEN <u1252970@umail.utah.edu>
2024-08-24 23:33:51 -0600

    finished create form template

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { provideHttpClient } from '@angular/common/http';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { CreateEmployeeComponent } from './create-employee/create-employee.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent]
--- angular-frontend/src/app/create-employee/create-employee.component.ts
+++ angular-frontend/src/app/create-employee/create-employee.component.ts
@@ -12,6 +12,7 @@ export class CreateEmployeeComponent implements OnInit {
   constructor() {};
   
   ngOnInit(): void {
-    
   }
+  
+  onSubmit(){};
 }
